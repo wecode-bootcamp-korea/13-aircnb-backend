53c6947504eb17d861193aab3d5e9139b2c9fd14
adriankim <adriankim@Adrians-MacBook-Pro.local>
2020-11-03 19:49:17 +0900

    set models.py

---
+++ reservations/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Booking(models.Model) :
+	stay 		 = models.ForeignKey('Stays', on_delete = models.CASCADE)
+	user 		 = models.ForeignKey('Users', on_delete = models.CASCADE)
+	checkin_date = models.DateField(auto_now=False, auto_now_add=False)
+	checkout_date= models.DateField(auto_now=False, auto_now_add=False)
+	guests 		 = models.IntegerField()
+	price 		 = models.IntegerField()
+	creditcard 	 = models.CharField(max_length=200)
+	request_date = models.DateField(auto_now=False, auto_now_add=False)
+	accept_date  = models.DateField(auto_now=False, auto_now_add=False)
+
+
+class Cancellation(models.Model) :
+	booking 	= models.ForeignKey('Booking', on_delete = models.CASCADE)
+	user 		= models.ForeignKey('Users', on_delete = models.CASCADE)
+	cancel_date = models.DateField(auto_now=False, auto_now_add=False)
