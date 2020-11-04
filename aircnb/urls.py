1ab8dd855965494b64e9d7b91fbd46c12935aad2
adriankim <adriankim@Adrians-MacBook-Pro.local>
2020-11-04 17:05:55 +0900

    Added user models

--- aircnb/urls.py
+++ aircnb/urls.py
@@ -1,9 +1,5 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('users', include('users.urls')),
-    path('stays', include('stays.urls')),
-    path('reservations', include('reservations.urls')),
-    path('wishlists', include('witshlists.urls')),
-    path('reviews', include('reviews.urls')),
+
 ]
